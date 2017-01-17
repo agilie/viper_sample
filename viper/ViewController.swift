afcb9c6091ea5bcf2e4c5eee65a872dd09214a06
Sergii Avilov <sergii.avilov@agilie.com>
2017-01-17 17:26:37 +0200

    Initial Commit

---
+++ viper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  viper
+//
+//  Created by Sergii Avilov on 1/17/17.
+//  Copyright © 2017 test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
