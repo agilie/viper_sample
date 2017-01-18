3f46c14b3f06f4aefe4e906e82d5ba66a3130cfb
Sergii Avilov <sergii.avilov@agilie.com>
2017-01-18 16:49:07 +0200

    Updated name

--- viper/Presentation/UserStories/StartScreen/View/StartScreenViewController.swift
+++ viper/Presentation/UserStories/StartScreen/View/StartScreenViewController.swift
@@ -2,7 +2,7 @@
 //  StartScreenViewController.swift
 //  viper
 //
-//  Created by Wirex on 18/01/2017.
+//  Created by Sergii Avilov on 18/01/2017.
 //  Copyright © 2017 Agilie. All rights reserved.
 //
 
